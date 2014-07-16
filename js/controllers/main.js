1900085db8ed18154ec5e77cc71b70e8602bae78
Rodrigo Soria <rodsilver83@hotmail.com>
2014-07-16 15:49:35 -0500

    key event ok

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -1,39 +1,43 @@
+var pageCtrl;
+
 (function(){
   var page = angular.module('page',[]);
 
-  page.controller('pageController',['$scope','$http',function($scope,$http) {
+  pageCtrl = page.controller('pageController',['$scope',function($scope) {
 
-    this.left = 18;
-    this.leftpointer = 18;
+    $scope.leftStyle={'margin-left':'18px'};
+    $scope.leftPointerStyle={'margin-left':'18px'};
+
+    var pageIndex = 0;
 
     this.home = function(){
-      this.leftpointer =18;
+      $scope.leftPointerStyle={'margin-left':'18px'}
+      pageIndex = 0;
       $.fn.fullpage.moveTo(0, 0);
     };
     this.aboutme = function(){
-      this.leftpointer =97;
+      $scope.leftPointerStyle={'margin-left':'97px'}
+      pageIndex = 1;
       $.fn.fullpage.moveTo(0, 1);
     };
     this.projects = function(){
-      this.leftpointer =192;
+      $scope.leftPointerStyle={'margin-left':'192px'}
+      pageIndex = 2;
       $.fn.fullpage.moveTo(0, 2);
     };
-    this.blog = function(){
-      this.leftpointer =273;
+    this.contact = function(){
+      $scope.leftPointerStyle={'margin-left':'290px'}
+      pageIndex = 3;
       $.fn.fullpage.moveTo(0, 3);
     };
-    this.contact = function(){
-      this.leftpointer =290;
-      $.fn.fullpage.moveTo(0, 3);
+    this.blog = function(){
+      $scope.leftPointerStyle={'margin-left':'350px'}
     };
     $scope.movepointer = function(position){
-      console.log(position);
-      this.left = position;
-      this.leftpointer = position;
+      $scope.leftPointerStyle={'margin-left': position+'px'}
+      $scope.leftStyle={'margin-left': position+'px'}
+      $scope.$apply();
     };
-
-
-
   }]);
 
 
@@ -45,11 +49,18 @@ $(document).ready(function () {
     verticalCentered: false,
     resize: false,
     afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
-      console.log(slideIndex)
       switch(slideIndex){
         case 0:
           angular.element(document.getElementById('pageController')).scope().movepointer(18);
-          angular.element(document.getElementById('pageController')).scope().$apply();
+          break;
+        case 1:
+          angular.element(document.getElementById('pageController')).scope().movepointer(97);
+          break;
+        case 2:
+          angular.element(document.getElementById('pageController')).scope().movepointer(192);
+          break;
+        case 3:
+          angular.element(document.getElementById('pageController')).scope().movepointer(290);
           break;
       }
     }
